bcb87cfd3213de5ee5f82b87bd94caa626b1c98c
Petra Chocholova <petra.chocholova@gmail.com>
2017-07-17 17:11:41 +0200

    Cat picture added with red dashed line around it

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+set :session_secret, 'super secret'
 
 get '/' do
   "Hello World - here I am"
@@ -8,4 +9,8 @@ get '/secret' do
   'This is a secret page'
 end
 
-set :session_secret, 'super secret'
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
